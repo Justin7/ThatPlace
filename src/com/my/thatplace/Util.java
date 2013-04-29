f8b4eb0aca3c984987073cbb58a60f234f68dad0
Justin7 <Justin7@gmail.com>
2013-04-29 23:20:46 +0900

    new java

---
+++ src/com/my/thatplace/Util.java
@@ -0,0 +1,8 @@
+package com.my.thatplace;
+
+public class Util {
+	public static final int OK = 1;
+	public static final int NG = 2;
+	public static final int NORMAL = 1;
+	public static final int ONLY_PLACE = 2;
+}
